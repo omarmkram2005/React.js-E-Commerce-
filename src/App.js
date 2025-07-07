6c470a7e0679e72ffd22a2d6d2214820bb71a623
omar mkram <mkramwolf@gmail.com>
2025-07-07 18:58:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import HomePage from "./Pages/Website/HomePage";
+import Users from "./Pages/Dashboard/Users";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GoogleCallback from "./Pages/Auth/GoogleCallback";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import ReqiureAuth from "./Pages/Auth/ReqiureAuth";
+import User from "./Pages/Dashboard/User";
+import AddUser from "./Pages/Dashboard/AddUser";
+import Err404 from "./Pages/Auth/Err404";
+import ReqiureBack from "./Pages/Auth/ReqiureBack";
+import Catigories from "./Pages/Dashboard/Categories";
+import AddCategory from "./Pages/Dashboard/AddCategory";
+import Category from "./Pages/Dashboard/Category";
+import Products from "./Pages/Dashboard/Products";
+import AddProduct from "./Pages/Dashboard/AddProduct";
+import Product from "./Pages/Dashboard/Product";
+import ShowAllCats from "./Pages/Website/ShowAllCats";
+import Website from "./Pages/Website/Website";
+import ProductPage from "./Components/ProductPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route element={<Website />}>
+          <Route path="" element={<HomePage />}></Route>
+          <Route path="/product/:id" element={<ProductPage />}></Route>
+          <Route path="/categories" element={<ShowAllCats />}></Route>
+        </Route>
+        <Route element={<ReqiureBack />}>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Route>
+        <Route
+          path="/auth/google/callback"
+          element={<GoogleCallback />}></Route>
+        <Route element={<Err404 />} path="/*"></Route>
+
+        <Route element={<ReqiureAuth allowedRole={["1995", "1996", "1999"]} />}>
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route element={<ReqiureAuth allowedRole={["1995"]} />}>
+              <Route path="users" element={<Users />}></Route>
+              <Route path="users/:id" element={<User />}></Route>
+              <Route path="user/add" element={<AddUser />}></Route>
+            </Route>
+            <Route element={<ReqiureAuth allowedRole={["1999", "1995"]} />}>
+              <Route path="categories" element={<Catigories />}></Route>
+              <Route path="categories/:id" element={<Category />}></Route>
+              <Route path="category/add" element={<AddCategory />}></Route>
+
+              <Route path="products" element={<Products />}></Route>
+              <Route path="products/:id" element={<Product />}></Route>
+              <Route path="product/add" element={<AddProduct />}></Route>
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Route } from "react-router-dom";
+import "./Css/componants/button.css";
+import "./Css/componants/alerts.css";
+import "./Css/componants/loading.css";
+import "./Pages/Auth/Auth.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import MenuContext from "./Context/MenuContext";
+import WindowContext from "./Context/WindowContext";
+// import UserProvider from "./Context/UserContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WindowContext>
+      <MenuContext>
+        <Route>
+          <App />
+        </Route>
+      </MenuContext>
+    </WindowContext>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
